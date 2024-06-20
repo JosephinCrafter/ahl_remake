3b7b14c761099141d3be78ebe659b8031f448631
Josephin Razafindrakoto <razafindrakotojosephin@gmail.com>
2024-06-21 02:07:28 +0300

    add donation page

---
+++ lib/src/pages/homepage/donation/donationpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DonationPage extends StatefulWidget {
+  const DonationPage({super.key});
+
+  static const String routeName = "/helpUs";
+
+  @override
+  State<DonationPage> createState() => _DonationPageState();
+}
+
+class _DonationPageState extends State<DonationPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
