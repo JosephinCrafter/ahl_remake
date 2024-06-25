c553aa4c70bbeb6ca0981f6e6514e06d20f61521
Josephin Razafindrakoto <razafindrakotojosephin@gmail.com>
2024-06-25 17:39:48 +0300

    remove flutter binding since we don't await firebase

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ import 'src/firebase_constants.dart';
 
 void main() async {
   // ensure flutter is initialized
-  WidgetsFlutterBinding.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
 
   // Make the app url based
   usePathUrlStrategy();
@@ -22,7 +22,7 @@ void main() async {
 
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
-  await settingsController.loadSettings();
+  settingsController.loadSettings();
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
@@ -31,7 +31,7 @@ void main() async {
   // here we go with firebase setup.
   // It is better to await this flutter app, but when working offline, it makes
   // the app not loading.
-  await firebaseApp;
+  firebaseApp;
 
   runApp(
     ChangeNotifierProvider.value(
