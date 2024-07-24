cd6ce049cf9b9eec3b9327e5e865592945c211d7
Josephin Razafindrakoto <razafindrakotojosephin@gmail.com>
2024-07-24 15:24:13 +0300

    implement prayers page

--- lib/src/pages/prayers/prayers_page.dart
+++ lib/src/pages/prayers/prayers_page.dart
@@ -1,5 +1,15 @@
-import 'package:ahl/src/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'package:gap/gap.dart';
+
+import 'package:ahl/src/newsletter/newsletter.dart';
+import 'package:ahl/src/prayers_space/view.dart';
+import 'package:ahl/src/widgets/widgets.dart';
+import '../../ahl_barrel.dart';
+import '../../utils/breakpoint_resolver.dart';
+import '../projects/projects_page.dart';
 
 class PrayersPage extends StatefulWidget {
   const PrayersPage({super.key});
@@ -15,11 +25,46 @@ class _PrayersPageState extends State<PrayersPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AhlAppBar(),
-      body: Center(
-        child: Text(
-          'Prayers Space',
-          style: Theme.of(context).textTheme.displayLarge,
-        ),
+      body: ListView(
+        children: [
+          const PrayerSpaceView(),
+          Gap(resolveSeparatorSize(context)),
+          const AhlDivider.symmetric(
+            space: 25,
+          ),
+          Gap(resolveSeparatorSize(context)),
+          SuggestionSection(
+            callback: () => Navigator.pushNamed(context, PrayersPage.routeName),
+            image: Future.value(
+              AssetImage(
+                AhlAssets.prayersSpaceCover,
+              ),
+            ),
+            child: Text(
+              AppLocalizations.of(context)!.prayerSpace,
+            ),
+          ),
+          Gap(resolveSeparatorSize(context)),
+          SuggestionSection(
+            callback: () =>
+                Navigator.pushNamed(context, ProjectsPage.routeName),
+            image: Future.value(
+              AssetImage(
+                AhlAssets.projectSpaceCover,
+              ),
+            ),
+            child: Text(
+              AppLocalizations.of(context)!.projectSpaceTitle,
+            ),
+          ),
+          Gap(resolveSeparatorSize(context)),
+          const AhlDivider.symmetric(
+            space: 25,
+          ),
+          Gap(resolveSeparatorSize(context)),
+          const NewsLetterPrompt(),
+          const AhlFooter(),
+        ],
       ),
     );
   }
